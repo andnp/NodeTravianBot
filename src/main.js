c9411685df58b922a0894e5529507d0ee790e8eb
andy <andnpatt@indiana.edu>
2016-01-18 21:41:56 -0500

    set up bot to handle multiple servers

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 //-------Global Variables--------
 
 global.server = "http://ts2.travian.us";
+global.serverName = "ts2";
+global.serverStart = new Date(2016,1,8);
 
 //-------------------------------
 var wio = require('webdriverio');
@@ -12,18 +14,20 @@ var login = require('./browser/login.js')(client);
 var auction = require('./browser/Auction.js')(client);
 
 var taskQueue = [];
+global.done = true;
 
 client.login().then(function(){
 	// begin execution of tasks in queue
 	function executor(){
-		if(taskQueue.length > 0){
+		if(taskQueue.length > 0 && global.done){
+			global.done = false;
 			taskQueue.shift()();
 		}
-		setTimeout(executor, 1000);
+		setTimeout(executor, 5000);
 	}
 
 	// client.placeAuctionBid(7, 500);
 	executor();
-	taskQueue.push(function(){require('./controllers/mapScanner.js')(client, taskQueue);});
+	taskQueue.push(function(){require('./controllers/mapScanner.js')(client, taskQueue); global.done = true;});
 	require('./controllers/auctionMarket.js')(client, taskQueue);
 });
